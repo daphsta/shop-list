63195448812ab605ec44a0f2ead4807bcba14e6d
yiingshan <yiingshan@gmail.com>
2014-03-04 11:58:36 +0000

    initial

---
+++ .rvm/gems/ruby-2.0.0-p247/gems/atomic-1.1.15/ext/siteconf20140304-577-1cvho2u.rb
@@ -0,0 +1,6 @@
+require 'rbconfig'
+dest_path = "./.gem.20140304-577-1dk4ws4"
+RbConfig::MAKEFILE_CONFIG['sitearchdir'] = dest_path
+RbConfig::CONFIG['sitearchdir'] = dest_path
+RbConfig::MAKEFILE_CONFIG['sitelibdir'] = dest_path
+RbConfig::CONFIG['sitelibdir'] = dest_path
---
+++ shop-list/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ShopList::Application.config.secret_key_base = '4087cdd930e1e6944c1701164e156dd443045454203c80a7401f930139fd7c4d4c7265f47c0b71a40c127de031551dccc5d6ffa4bba489af5530917c193e371c'
